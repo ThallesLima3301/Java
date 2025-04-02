7168b15af22d98d04d2bdc027cd9ec198cba4314
Thalles <70488595+ThallesLima3301@users.noreply.github.com>
2025-04-01 22:33:46 -0300

    iPhone.java

---
+++ iPhone/Main.java
@@ -0,0 +1,10 @@
+package iPhone;
+
+public class Main {
+    public static void main(String[] args) {
+        iPhone meuIphone = new iPhone();
+        meuIphone.tocar();
+        meuIphone.ligar("123456789");
+        meuIphone.exibirPagina("https://www.google.com");
+    }
+}
